564998898f133ba2093ec48b74ed1c4b54754079
Antonin ROSA-MARTIN <antonin.martin.rosa@gmail.com>
2020-10-29 01:19:41 +0100

    We now have a dynmap with times and different level of zoom

--- hytalemonitoringapi/routes/index.js
+++ hytalemonitoringapi/routes/index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 
 const PNG = require('pngjs').PNG;
 
+let imageSize = 384;
+
 let images = {};
 
 fs.createReadStream('./public/ressources/0.png')
@@ -37,51 +39,43 @@ fs.createReadStream('./public/ressources/4.png')
         images["4"] = this;
     });
 
+// z: 1 --> 1 * 1 chunk, 2 --> 2 * 2 chunk ... 11 --> 11 * 11 chunks
+
+const levelOfZoom = 8;
+// The levels of zoom is 16 that means that when z=1 we can have 2^16 = 65536 that is to say the span of x is [-2^15 -1, 2^15] like signed integers
+// That is to say for a given z the x range is [-2^levelOfZoom-1 -1, 2^levelOfZoom-1] and x should be shifted to -2^levelOfZoom-1 -1 to be centered to 0
 
 router.get('/api/tile/:x/:y/:z', ((req, res) => {
 
     let fs = require('fs');
     let PNG = require('pngjs').PNG;
 
-    let chunk = [...Array(16 * 16).keys()].map(() => getRandomInteger(0, 4));
+    let x = +req.params.x;
+    let y = +req.params.y;
+    let z = +req.params.z;
 
-    let dst = new PNG({width: 64 * 16, height: 64 * 16});
+    // We shift x and y to the middle
+    x += (-Math.pow(2, levelOfZoom - z) - 1);
+    y += (-Math.pow(2, levelOfZoom - z) - 1);
 
-    chunk.forEach((d, i) => {
+    // We create a destination image 512px by 512px
+    let dst = new PNG({width: 1024 * z, height: 1024 * z});
 
-        // Draw the images into the chunk image
-        images[d].bitblt(dst, 0, 0, 64, 64, (i % 16) * 64, (Math.floor(i/16)) * 64);
-    })
+    let chunk;
+    for (let nbChunkX = 0; nbChunkX < z; nbChunkX++) {
+        for (let nbChunkY = 0; nbChunkY < z; nbChunkY++) {
+            chunk = [...Array(16 * 16).keys()].map(() => getRandomInteger(0, 4));
 
-    function resize(srcPng, width, height) {
-        var rez = new PNG({
-            width:width,
-            height:height
-        });
-        for(var i = 0; i < width; i++) {
-            var tx = i / width,
-                ssx = Math.floor(tx * srcPng.width);
-            for(var j = 0; j < height; j++) {
-                var ty = j / height,
-                    ssy = Math.floor(ty * srcPng.height);
-                var indexO = (ssx + srcPng.width * ssy) * 4,
-                    indexC = (i + width * j) * 4,
-                    rgbaO = [
-                        srcPng.data[indexO  ],
-                        srcPng.data[indexO+1],
-                        srcPng.data[indexO+2],
-                        srcPng.data[indexO+3]
-                    ]
-                rez.data[indexC  ] = rgbaO[0];
-                rez.data[indexC+1] = rgbaO[1];
-                rez.data[indexC+2] = rgbaO[2];
-                rez.data[indexC+3] = rgbaO[3];
-            }
+            chunk.forEach((d, i) => {
+
+                // Draw the images into the chunk image
+                images[d].bitblt(dst, 0, 0, 64, 64, (nbChunkX * 1024) + (i % 16) * 64, (nbChunkY * 1024) + (Math.floor(i/16)) * 64);
+            })
+
         }
-        return rez;
     }
 
-    resize(dst, 512, 512).pack().pipe(res);
+    resize(dst, imageSize, imageSize).pack().pipe(res);
 
     /*
     fs.createReadStream('./public/ressources/0.png')
@@ -98,6 +92,35 @@ router.get('/api/tile/:x/:y/:z', ((req, res) => {
      */
 
 }));
+
+
+function resize(srcPng, width, height) {
+    var rez = new PNG({
+        width:width,
+        height:height
+    });
+    for(var i = 0; i < width; i++) {
+        var tx = i / width,
+            ssx = Math.floor(tx * srcPng.width);
+        for(var j = 0; j < height; j++) {
+            var ty = j / height,
+                ssy = Math.floor(ty * srcPng.height);
+            var indexO = (ssx + srcPng.width * ssy) * 4,
+                indexC = (i + width * j) * 4,
+                rgbaO = [
+                    srcPng.data[indexO  ],
+                    srcPng.data[indexO+1],
+                    srcPng.data[indexO+2],
+                    srcPng.data[indexO+3]
+                ]
+            rez.data[indexC  ] = rgbaO[0];
+            rez.data[indexC+1] = rgbaO[1];
+            rez.data[indexC+2] = rgbaO[2];
+            rez.data[indexC+3] = rgbaO[3];
+        }
+    }
+    return rez;
+}
 
 
 /*
--- hytalemonitoringreact/src/components/MapSVG.js
+++ hytalemonitoringreact/src/components/MapSVG.js
@@ -15,19 +15,23 @@ class MapSVG extends Component {
         const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
         const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
+        let imageSize = 384;
+
         const svg = d3.select("#rootMap").append("svg")
+            .style('background-color', "#1a1a1a")
             .attr("viewBox", [0, 0, width, height]);
 
         const tile = d3tile.tile()
             .extent([[0, 0], [width, height]])
-            .tileSize(512)
+            .tileSize(imageSize)
             .clampX(true)
             .clampY(true);
 
+        // We want 16 levels of zoom thus
+        let levelsOfZoom = 8;
+        let initialZoomLevel = 1;
         const zoom = d3.zoom()
-
-            // Now i get from z = 0 to z = 10
-            .scaleExtent([512, 512000])
+            .scaleExtent([imageSize, imageSize * (Math.pow(2, levelsOfZoom))])
             .extent([[0, 0], [width, height]])
             .on("zoom", ({transform}) => zoomed(transform));
 
@@ -35,23 +39,31 @@ class MapSVG extends Component {
             .attr("pointer-events", "none")
             .selectAll("image");
 
-        let url = (x, y, z) => `/api/tile/${x}/${y}/${z}`;
+        // The URL is the x and the y with a "reversed" z from 1 to 11 where 1 1:1 chunk and 11 is 1:11
+        let url = (x, y, z) => `/api/tile/${x}/${y}/${(levelsOfZoom - z) + 1}`;
 
         svg
             .call(zoom)
             .call(zoom.transform, d3.zoomIdentity
                 .translate(width >> 1, height >> 1)
-                .scale(65536));
+
+                // We want the default zoom to levelsOfZoom - initialZoomLevel + 1
+                .scale(imageSize * Math.pow(2, levelsOfZoom - initialZoomLevel + 1)));
 
         function zoomed(transform) {
+
             const tiles = tile(transform);
 
             image = image.data(tiles, d => d).join("image")
-                .attr("xlink:href", d => url(...d3tile.tileWrap(d)))
+                .attr("href", d => url(...d3tile.tileWrap(d)))
                 .attr("x", ([x]) => (x + tiles.translate[0]) * tiles.scale)
                 .attr("y", ([, y]) => (y + tiles.translate[1]) * tiles.scale)
-                .attr("width", tiles.scale)
-                .attr("height", tiles.scale);
+                .style('opacity', '1')
+                .classed('fade-in', true)
+
+                // We add a little bit of width and height to have better edges and then no borders (is it because of the antialiasing ???)
+                .attr("width", tiles.scale + 0.5)
+                .attr("height", tiles.scale + 0.5);
         }
     }
 
