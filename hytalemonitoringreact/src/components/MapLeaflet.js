bc8d948b77c5e314a95d81d55cdd98138cde9bb0
Antonin ROSA-MARTIN <antonin.martin.rosa@gmail.com>
2020-11-18 00:47:03 +0100

    We now have a map where the coordinate system is mapped with the lat and the lng. We can also draw chunks

--- hytalemonitoringreact/src/components/MapLeaflet.js
+++ hytalemonitoringreact/src/components/MapLeaflet.js
@@ -37,6 +37,8 @@ class MapLeaflet extends Component {
 
     initMap = () => {
 
+        let instance = this;
+
         L.TileLayer.MyCustomLayer = L.TileLayer.extend({
             getTileUrl: function(coords) {
 
@@ -68,7 +70,7 @@ class MapLeaflet extends Component {
         }
 
         // We create the map
-        map = L.map('mapid').setView([0, 0], 17);
+        map = L.map('mapid').setView([0, 0], 15);
 
         // We add the tileLayer which is connected to our API (Where we will be only able to have 6 levels of zoom from -1 to -6)
         // L.tileLayer.myCustomLayer('/api/tile/{x}/{y}/{z}', {
@@ -84,6 +86,12 @@ class MapLeaflet extends Component {
 
         // We change the text of the right side of the screen
         document.getElementsByClassName('leaflet-control-attribution')[0].innerHTML = '<span>Made with <span style="color: red">❤</span> by <a href="localhost" title="Website to monitor your Hytale server">HytaleMonitoringProject</a> with <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a></span>';
+
+        // Add the coordinates box
+        document.querySelector(".leaflet-bottom.leaflet-left").innerHTML = `<div class="coordinates-box leaflet-control"><span id="coordinates-span">Coordinates: </span></div>`
+
+        // Set the default coordinates
+        instance.changeCoordinates(10, 255)
 
         // Get the current zoom value as it is sent to the server
         const getZoom = () => {
@@ -103,38 +111,107 @@ class MapLeaflet extends Component {
         // This is used to get the coordinate
         map.addEventListener('mousemove', function(event) {
 
+            // Get the lat and the lng of the mouse
             let lat = event.latlng.lat;
             let lng = event.latlng.lng;
 
-            console.log(getZoom())
+            // Get the world coordinate of the cursor
+            const [x, z] = instance.getWorldPositionFromLatLng(lat, lng)
 
-            let x = lat * Math.pow(2, map.getZoom() - 1);
-            let y = lng * Math.pow(2, map.getZoom() - 1);
-
-            // console.log(x, y)
+            // Change the coordinates displayed
+            instance.changeCoordinates(Math.floor(x), Math.floor(z))
         });
     }
 
-    // Highlight a given chunk
-    highlightChunk = (chunk) => {
+    // Get the current zoom value as it is sent to the server
+    getZoom = () => {
 
-        /*
-        var circle = L.rectangle([[0, 0], [0, 16], [16, 0], [16, 16]], {
-            color: 'red',
-            fillColor: '#f03',
+        // Get the zoom level of the map
+        let zoomLevel = map.getZoom();
+
+        // We check if the zoom calculation is reversed
+        if (options.zoomReverse) {
+            zoomLevel = options.maxZoom - zoomLevel;
+        }
+
+        // We return the zoom with the offset
+        return zoomLevel + 1;
+    };
+
+    getWorldPositionFromLatLng = (lat, lng) => {
+        let point = map.project(L.latLng(lat, lng), map.getZoom());
+
+        // Get the x and z relative to the tile size in pixel
+        let x = point.x / options.tileSize;
+        let z = point.y / options.tileSize;
+
+        // The is used not to calculate this value each time
+        let twoPowZMinusOne = Math.pow(2, this.getZoom() - 1);
+
+        // Shift the x and z
+        x -= (1 << map.getZoom() - 2);
+        z -= (1 << map.getZoom() - 2);
+
+        // And apply the "ratio"
+        x *= 16 * twoPowZMinusOne;
+        z *= 16 * twoPowZMinusOne;
+
+        return [x, z];
+    }
+
+    getLatLngFromWorldPosition = (x, z) => {
+
+        // The is used not to calculate this value each time
+        let twoPowZMinusOne = Math.pow(2, this.getZoom() - 1);
+
+        // Apply the "ratio"
+        x /= 16 * twoPowZMinusOne;
+        z /= 16 * twoPowZMinusOne;
+
+        // Shift the x and z
+        x += (1 << map.getZoom() - 2);
+        z += (1 << map.getZoom() - 2);
+
+        // Get the x and z relative to the tile size in pixel
+        let point = L.point(x * options.tileSize, z * options.tileSize);
+
+        // Get the latlng
+        let latlng = map.unproject(point, map.getZoom());
+
+        // Return the result
+        return [latlng.lat, latlng.lng]
+    }
+
+    changeCoordinates = (x, z) => {
+        document.getElementById("coordinates-span").innerHTML = `Coordinates: (<strong>X</strong>:${x}, <strong>Z</strong>:${z}`
+    }
+
+    // Highlight a given chunk
+    highlightChunkLatLng = (lat1, lng1, lat2, lng2, color = "red") => {
+        L.rectangle([[lat1, lng1], [lat2, lng2]], {
+            color: color,
+            fillColor: color,
             fillOpacity: 0.5,
-            radius: 500,
-            width: 100,
-            height: 100
         }).addTo(map);
-         */
+    };
+
+    highlightChunkWorldPosition = (x1, y1, x2, y2, color = "red") => {
+
+        let latLng1 = this.getLatLngFromWorldPosition(x1, y1)
+        let latLng2 = this.getLatLngFromWorldPosition(x2, y2)
+
+        L.rectangle([latLng1, latLng2], {
+            color: color,
+            fillColor: color,
+            fillOpacity: 0.5,
+        }).addTo(map);
     };
 
     // When the component has been mounted we can proceed with the map
     componentDidMount() {
         this.initMap();
 
-        this.highlightChunk({x: 0, y: 0});
+        this.highlightChunkWorldPosition(0, 0, 31, 31, "white")
     }
 
     render() {
